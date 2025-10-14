758e6f4a69660853177f668396b20578f4a1df6b
blazejzj <opexrrator@gmail.com>
2025-10-14 13:01:43 +0200

    fix: resolved bug where submitting register form crashed the app, wrangler didnt find migrations dir

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/libsql";
 import { env } from "cloudflare:workers";
 import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/d1";
 
 const db = drizzle(env.DB, { schema });
 
