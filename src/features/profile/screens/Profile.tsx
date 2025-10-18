93307d454ff50745128229b584576efaab46e556
Stefan Strand <stefanboytchev@hotmail.com>
2025-10-18 13:25:57 +0200

    feature(profile): Times up, just minor styling alterations. Basic layout and structure for profilePage seems ok now.

--- src/features/profile/screens/Profile.tsx
+++ src/features/profile/screens/Profile.tsx
@@ -14,17 +14,17 @@ export default function Profile() {
         //Sidebar componetn goes here: unsure of exact placement (within/witout main)
         <div className="flex min-h-screen">
             <SideBar />
-            <main className="mx-auto max-w-4xl p-6">
+            <main className="flex-1 p-6">
                 {/* Header section here */}
-                <section className="bg-violet-400 border-b">
-                    <h1 className="mb-4 text-4xl font-bold ">
+                <section className=" border-b">
+                    <h1 className="mb-4 text-4xl font-bold">
                         Profile Settings
                     </h1>
                     {/* Need separation line here, vert and horizontaøl. */}
                 </section>
 
                 {/* ProfilePic section here */}
-                <section className="mb-6 mt-6 flex items-center gap-4 bg-amber-600">
+                <section className="mb-6 mt-6 flex items-center gap-4">
                     <div className="h-20 w-20 rounded-full bg-gray-300" />
                     <button
                         type="button"
