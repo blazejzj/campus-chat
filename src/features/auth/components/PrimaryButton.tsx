9c13c55bb8bbb7c3805a4e3c85e8495db0a214d5
blazejzj <opexrrator@gmail.com>
2025-11-27 19:41:55 +0100

    fix: changed PrimaryButton to include cursor pointer (imporant! :))

--- src/features/auth/components/PrimaryButton.tsx
+++ src/features/auth/components/PrimaryButton.tsx
@@ -7,7 +7,7 @@ export default function PrimaryButton({ disabled, children }: Props) {
     return (
         <button
             disabled={disabled}
-            className="w-full rounded-xl theme-bg-color text-white py-2.5 font-semibold hover:opacity-90 transition disabled:opacity-60"
+            className="w-full rounded-xl theme-bg-color text-white py-2.5 font-semibold hover:opacity-90 transition disabled:opacity-60 cursor-pointer"
         >
             {children}
         </button>
